1cc8d2ea8ebbd65880094497a0df077227430dbd
henryXu <320853112@qq.com>
2020-08-25 00:16:52 +0800

    8.25

--- src/main/java/com/xu/controller/TestController.java
+++ src/main/java/com/xu/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.xu.controller;
 
+import com.xu.domain.User;
 import com.xu.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -18,4 +21,11 @@ public class TestController {
         userService.saveAllUser();
         return "ok";
     }
+
+    @RequestMapping("/testSql")
+    public Object testSql(){
+        List<User> users = userService.emSave();
+        return users;
+    }
+
 }
--- src/main/java/com/xu/service/UserService.java
+++ src/main/java/com/xu/service/UserService.java
@@ -3,15 +3,27 @@ package com.xu.service;
 
 import com.xu.DAO.UserRepository;
 import com.xu.domain.User;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.sql.DataSource;
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
+
+    @PersistenceContext
+    EntityManager entityManager;
 
     public void saveAllUser(){
         ArrayList list = new <User>ArrayList();
@@ -19,5 +31,38 @@ public class UserService {
         list.add(new User("u2","p2"));
         list.add(new User("u3","p3"));
         userRepository.saveAll(list);
+    }
+
+    @Transactional
+    public List<User> emSave(){
+        ArrayList<User> list = new <User>ArrayList();
+        list.add(new User("u1","p1"));
+        list.add(new User("u2","p2"));
+        list.add(new User("u3","p3"));
+        StringBuilder sql=new StringBuilder();
+        sql.append("insert into test1 values");
+        int size = list.size();
+        if(size>0){
+            for (User u : list) {
+                sql.append(getValue(u));
+            }
+        }
+        sql.deleteCharAt(sql.length()-1);
+//        entityManager.getTransaction().begin();
+        //创建原生查询的时候，将info.class类即第二个参数，写成要传回的bean，这样就可以直接用List<Bean>接收
+//        Query query = entityManager.createQuery (sql.toString());
+//        DataSource dataSource = entityManager.unwrap(DataSource.class);
+        Session session = entityManager.unwrap(org.hibernate.Session.class);
+        session.createQuery(sql.toString()).executeUpdate();
+//        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+//        jdbcTemplate.execute(sql.toString());
+        return null;
+    }
+
+    private String getValue(User user ){
+        //('u1','p4'),
+        StringBuilder value = new StringBuilder();
+        value.append("('").append(user.getUsername()).append("','").append(user.getPassword()).append("'),");
+        return value.toString();
     }
 }
